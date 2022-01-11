08232fdd513ddbdf48e3f540e9ec5c51db7841b8
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-01-11 12:43:42 -0300

    rev.geral

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StateProvider } from './src/contexts/StateContext';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import AuthStack from './src/stacks/AuthStack';
+
+export default () => {
+    return (
+        <StateProvider>
+            <NavigationContainer>
+                <AuthStack />
+            </NavigationContainer>
+        </StateProvider>    
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
